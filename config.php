4af2facc5fb37f145884e4db9ec9aa3b56153c7b
hkm-mo <hkmcss@gmail.com>
2017-02-07 17:38:13 +0800

    Add a config file to provide an example for user

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$view_config['view_path'] = './views/';
+$view_config['output_collapse'] = FALSE;
+$view_config['title_separator'] = ' - ';
+
+$view_config['js_debug'] = TRUE;
+$view_config['css_debug'] = TRUE;
+
+$view_config['minify_js_ext'] = '.min.js';
+$view_config['minify_css_ext'] = '.min.css';
+
+$view_config['magic_replace'] = array();
+$view_config['base_path'] = '/';
+$view_config['base_path_symbol'] = '~/';
